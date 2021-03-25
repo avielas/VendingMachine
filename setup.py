714efb759825b49d98695cf5768ee623cb19cfca
Korsensky, Sergey <sergey.korsensky@intel.com>
2021-03-25 12:50:13 +0200

    Repository set up

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 lPackages = find_packages(exclude=('tests', 'tests.*'))
 
 
-setup(name='template_home_assignment',
+setup(name='aviela_home_assignment',
     version='0.1.0',
-    description='Template home assignment',
+    description='Aviel A home assignment',
     author='na',
     author_email='na',
 
