aa8a3bcaa647008fbfbf699386abcb2b97a6586f
aviel <avielas@gmail.com>
2021-04-17 16:32:32 +0300

    add test to Calculator for None case

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@
 from aviela_home_assignment.calculator import Calculator
 
 
-def test_calculate_minimum():
+def test_calculate_minimum_returned_dictionary():
     calc = Calculator()
 
     coins = dict([])
@@ -17,5 +17,22 @@ def test_calculate_minimum():
     expectedRes = dict([('11', 3)])
     # calc.__CalculateFinite return {20: 1, 11: 1, 1: 2}
     # but calc.CalculateMinimum return {11: 3}
+    res = calc.CalculateMinimum(coins, amount)
+    assert expectedRes == res
+
+
+def test_calculate_minimum_returned_none():
+    calc = Calculator()
+
+    coins = dict([])
+    coins['10'] = 1
+    coins['5'] = 1
+    coins['2'] = 1
+    coins['1'] = 1
+
+    amount = 9
+
+    expectedRes = None
+    # there is no coins to produce the amount of change required
     res = calc.CalculateMinimum(coins, amount)
     assert expectedRes == res
