a713ccb1f33944364c82d948bed37ab4c36bf08c
aviel <avielas@gmail.com>
2021-04-25 20:50:46 +0300

    enforce python version > 3.8

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ lPackages = find_packages(exclude=('tests', 'tests.*'))
 
 
 setup(name='aviela_home_assignment',
+    python_requires='>3.8',
     version='0.1.0',
     description='Aviel A home assignment',
     author='na',
@@ -12,7 +13,7 @@ setup(name='aviela_home_assignment',
     packages=lPackages,
 
     install_requires=[
-        # 'enum34;python_version<"3.4"',
+        # 'enum34;python_version<"3.8"',
         'pytest', 'plantuml'
     ],
 )
